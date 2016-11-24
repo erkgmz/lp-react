ecd9e93c8f3f635f35459a09d4fa00dd22a90d56
Erik Gomez <egoxiv@gmail.com>
2016-11-23 18:05:28 -0800

    Replaces lorem ipsum with real copy

--- src/components/home/CopyBlock.js
+++ src/components/home/CopyBlock.js
@@ -1,13 +1,15 @@
 import React from 'react';
+import {Link} from 'react-router';
 
 const CopyBlock = () => {
   return (
     <p>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-      Phasellus efficitur nibh augue, ac malesuada justo tincidunt et.
-      Donec dictum nisl nisi, id accumsan nisi rutrum id. Nulla non
-      vehicula massa, at dapibus libero. Aenean id tortor nec sem
-      vehicula posuere eu eget er
+      I live in sunny Bakersfield, California. I am a voracious consumer of information, entrepreneurial spirit
+      and seasoned front-end developer. I frequently work with technologies like Angular.js and React.js for
+      front-end projects and and Node.js+Express and Mongoose.js on backend projects.
+      <br />
+      <br />
+      Want to work together? Got a question? Just <Link to="/contact">contact me</Link>.
     </p>
   );
 };
