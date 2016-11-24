9f4d38484527da380e81ee79cfbf41cb4a67c2d4
Erik Gomez <egoxiv@gmail.com>
2016-11-23 18:12:59 -0800

    null

--- src/components/home/CopyBlock.js
+++ src/components/home/CopyBlock.js
@@ -6,7 +6,7 @@ const CopyBlock = () => {
     <p>
       I live in sunny Bakersfield, California. I am a voracious consumer of information, entrepreneurial spirit
       and seasoned front-end developer. I frequently work with technologies like Angular.js and React.js for
-      front-end projects and and Node.js+Express and Mongoose.js on backend projects.
+      front-end projects and Node.js+Express and Mongoose.js on backend projects.
       <br />
       <br />
       Want to work together? Got a question? Just <Link to="/contact">contact me</Link>.
