77fdec4bedc7ef1a56c647f731f25afbedb40b86
Erik Gomez <egoxiv@gmail.com>
2017-01-19 08:32:20 -0800

    Updates flightplan

--- flightplan.js
+++ flightplan.js
@@ -8,18 +8,19 @@ var startFile = 'dist/';
 var tmpDir = appName+'-' + new Date().getTime();
 
 // configuration
-plan.target('staging', [
-  {
-    host: '104.131.128.185',
-    username: username,
-    agent: process.env.SSH_AUTH_SOCK
-  }
-]);
+// plan.target('staging', [
+//   {
+//     host: '104.131.128.185',
+//     username: username,
+//     agent: process.env.SSH_AUTH_SOCK
+//   }
+// ]);
 
 plan.target('production', [
   {
-    host: 'erikgomez.co',
+    host: '104.131.128.185',
     username: username,
+    privateKey: '/Users/erikgomez/.ssh/id_rsa',
     agent: process.env.SSH_AUTH_SOCK
   },
 //add in another server if you have more than one
@@ -32,7 +33,6 @@ plan.target('production', [
 
 // run commands on localhost
 plan.local(function(local) {
-  // ADD+COMMIT EVERYTHING FIRST SO /dist DIRECTORY CAN BE RSYNCED
   local.log('Copy files to remote hosts');
   var files = local.exec('git ls-files && ls .env', {silent: true});
   // rsync files to all the destination's hosts
@@ -50,9 +50,8 @@ plan.remote(function(remote) {
   remote.sudo('npm --production --prefix ~/' + tmpDir + ' install ~/' + tmpDir, {user: username});
 
   remote.log('Reload application');
-  remote.sudo('ln -snf ~/' + tmpDir + ' ~/'+appName, {user: username});// SYMLINK?
+  remote.sudo('ln -snf ~/' + tmpDir + ' ~/'+appName, {user: username});
 
-  // remote.exec('forever stop /' + appName, {failsafe: true});
   remote.exec('forever stopall', {failsafe: true});
   remote.exec('cd ~/' + appName + ' && forever start --minUptime 20000 --spinSleepTime 1000 -c "npm start" ./');
   // https://github.com/foreverjs/forever/issues/540
