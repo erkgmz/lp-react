6b1eac9fa75d4a5ce3454556d9e8c28739fa857a
Erik Gomez <egoxiv@gmail.com>
2017-02-04 15:20:58 -0800

    Complete work on interval feture that cycles through array of skills and re renders a string. Adds abstraction for skills.

--- src/components/home/CopyBlock.js
+++ src/components/home/CopyBlock.js
@@ -1,44 +1,27 @@
 import React, {Component} from 'react';
 import {Link} from 'react-router';
 
-const skills = [
-  'Babel',
-  'ES2015 / ES6',
-  'React Native',
-  'Express',
-  'Angular JS',
-  'Mongoose JS',
-  'Responsive Web Design',
-  'Sketch',
-  'UI design',
-  'UX design',
-  'Handlebars JS',
-  'JQuery',
-  'SASS',
-  'Git',
-  'Unit Testing',
-  'Continous Integration (CI)',
-  'Advanced CSS',
-  'Bootstrap',
-  'React',
-  'Webpack',
-  'Node JS',
-  'CSS Flexbox',
-  'Wordpress',
-  'Mocha',
-  'ES2016 / ES7'
-];
+import skills from './skills';
 
 class CopyBlock extends Component {
   constructor(props, context) {
     super(props, context);
 
     this.state = {
-      skill: skills[0]
+      currentSkill: skills[0]
     };
 
-    this.setRandomSkill = this.setRandomSkill.bind(this);
+    this.randomSkill = this.randomSkill.bind(this);
     this.randomNumber = this.randomNumber.bind(this);
+  }
+
+  componentWillMount() {
+    let intervalId = setInterval(this.randomSkill, 1000);
+    this.setState({intervalId: intervalId});
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.state.intervalId);
   }
 
   randomNumber() {
@@ -46,19 +29,16 @@ class CopyBlock extends Component {
     return random;
   }
 
-  setRandomSkill() {
-    this.setState({skill: skills[this.randomNumber()]});
-    // Still working on skills sequence
-    // probably need to integrate componentWillMount()
-    // current behavior seems to be snowballing the sequence
-    // example: it seems to add several string in state
-    // and doubles everytime it sets state
+  randomSkill() {
+    let rand = this.randomNumber();
+    let skill = skills[rand];
+    this.setState({ currentSkill: skill });
   }
 
   render() {
     return (
       <div>
-        <p>That includes ReactJS, NodeJS, Webpack and many other useful tools like {this.state.skill}</p>
+        <p>That includes ReactJS, NodeJS, Webpack and many other useful tools like {this.state.currentSkill}.</p>
 
         <p>I am a self taught web developer and General Assembly WDI alumni. I've worked for both big and small companies
           and have several years experience in the web development industry.</p>
