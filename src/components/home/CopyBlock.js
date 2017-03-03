6a389ac0fb747730ef5ae5029362a948f8824008
Erik Gomez <egoxiv@gmail.com>
2017-03-02 21:48:56 -0800

    Adds Kaileys project to portfolio

--- src/components/home/CopyBlock.js
+++ src/components/home/CopyBlock.js
@@ -51,20 +51,20 @@ class CopyBlock extends Component {
     return (
       <div className={this.props.containerStyle}>
         <Byline text="What I do" />
-        <p>I build modern web and mobile applications with JavaScript and technologies like {this.state.currentSkill}</p>
+        <p>I build modern, responsive web applications with JavaScript and technologies like {this.state.currentSkill}</p>
 
         <p>If you have any questions or need digital services, I am currently available for:</p>
         <ul>
           <li><p>Web development</p></li>
-          <li><p>User Experience Design (UX)</p></li>
+          <li><p>UX Design (UX)</p></li>
           <li><p>Site audits & redesigns</p></li>
           <li><p>Search Engine Optimization (SEO)</p></li>
-          <li><p>Mentorships for JavaScript newbies</p></li>
+          <li><p>Mentorships for JavaScript</p></li>
         </ul>
 
         <Byline text="A little more about me" />
 
-        <p>I am passionate about learning, development and technology. I am a General Assembly alumni with a  GitHub history goes back to 2014 where I first began coding by making static sites with HTML and CSS. I began working for <a href="https://bulletproof.com">Bulletproof 360</a> in 2014 and got on-the-job experience in content marketing, search engine optimization, and front-end development. Then I discovered a passion for coding, JavaScript in particular. I know that's a clichè but I actually mean it and here's why: </p>
+        <p>I am passionate about learning, development and technology. I am a General Assembly alumni with a  GitHub history that goes back to 2014 where I first began coding by making static sites with HTML and CSS. I began working for <a href="https://bulletproof.com">Bulletproof 360</a> in 2014 and got on-the-job experience in content marketing, search engine optimization, and a little front-end development. Then I discovered a passion for coding, JavaScript in particular. I know that's a clichè but I actually mean it and here's why: </p>
 
         <Byline text="I love to learn" />
 
@@ -72,17 +72,17 @@ class CopyBlock extends Component {
 
         <Byline text="General Assembly" />
 
-        <p>I realized it was time to "level-up" my JavaScript skills, so in early 2016 I quit my job, completed a ton of tutorials on <a href="https://teamtreehouse.com/">TeamTreehouse</a>, and enrolled into <a href="https://generalassemb.ly/education/web-development-immersive">General Assembly's WDI</a> (Web Development Immersive). The experience was intense: 14 hours of coding, six days a week, for 12 weeks, and it has changed my life.</p>
+        <p>I realized that in order to become the front-end developer I envisioned, I needed a greater challenge. In 2015, I did the unthinkable and quit my job and enrolled into <a href="https://generalassemb.ly/education/web-development-immersive">General Assembly's WDI</a> (Web Development Immersive). The experience was intense: 14 hours of coding, six days a week, for 12 weeks, and it has changed my life.</p>
 
-        <p>Immediately after graduating, I was selected for an internship as a software engineer at <a href="https://www.razorfish.com/">Razorfish</a>. There, I worked on a team of engineers and had the opportunity to flex my front-end skills on a live website in production for a global automobile manufacturer.</p>
+        <p>Immediately after graduating, I was selected for an internship as a software engineer at <a href="https://www.razorfish.com/">Razorfish</a>. There, I worked on a team of engineers and designers and had the opportunity to flex my front-end skills on a website in production for a global automobile manufacturer.</p>
 
         <Byline text="What about now?" />
 
-        <p>I am spending most of my time working on projects for clients and learning all the ins and outs of React.js.</p>
+        <p>I am spending most of my time working on projects for clients and learning the ins and outs of React.js.</p>
 
-        <p>If I'm not making websites or learning something new, I can usually be found hanging out with friends or doing something with my wife, Kailey, our daughter, Leah, and our English Bulldog, Tito. My health and fitness are important to me so I work out a few times a week and love to spend time outdoors, hiking, and biking.</p>
+        <p>If I'm not making websites or learning something new, I can usually be found hanging out with friends or doing something with my wife, Kailey, our daughter, Leah, or my dog, Tito. My health and fitness are important to me so I work out a few times a week and love to spend time outdoors, hiking, and biking.</p>
 
-        <p>I am a huge foodie! I love to try new recipes or unique foods, but I also appreciate a well put together meal, no matter how normal it is. The weirdest thing I've eaten to date are crickets (increasingly becoming less weird to eat, for some reason) and I am known to host a really good BBQ.</p>
+        <p>I also love to cook. I usually experiment with new recipes or make the one I am currently obsessed with. If I'm not cooking, I'm probably looking for a new restaurant to try, most likely a "mom and pop" with a great rating on Yelp.</p>
 
         <Byline text="Want more info?" />
         <p><Link to="/contact">Send me a message</Link> or connect with me.</p>
--- src/components/work/projects.js
+++ src/components/work/projects.js
@@ -1,16 +1,18 @@
 const PROJECTS = [
   {
-    title: 'Under Construction UI',
-    link: 'https://erkgmz.github.io/e-lp/',
-    noFollow: true,
+    title: 'KS MARKETING SERVICES',
+    link: 'https://goo.gl/XaPTjw',
+    techs: ['React', 'CSS Modules', 'ES6'],
+    noFollow: false,
     img: {
-      path: require('./portfolio_images/personalPage.png'),
-      alt: 'cover image'
+      path: require('./portfolio_images/ksMarketing.png'),
+      alt: 'team photo'
     }
   },
   {
     title: 'GA ADMISSIONS',
     link: 'https://ga-admissions.herokuapp.com/',
+    techs: ['Mongo', 'Express', 'Angular.js', 'Node.js', 'Bootstrap'],
     noFollow: false,
     img: {
       path: require('./portfolio_images/generalAssembly.png'),
@@ -20,10 +22,21 @@ const PROJECTS = [
   {
     title: 'STUD FINDER',
     link: 'http://course-walkers.herokuapp.com/',
+    techs: ['jQuery', 'Flexbox', 'Handlebars.js'],
     noFollow: false,
     img: {
       path: require('./portfolio_images/coursewalkers.png'),
       alt: 'Course Walkers logo'
+    }
+  },
+  {
+    title: 'UNDER CONSTRUCTION',
+    link: 'https://erkgmz.github.io/e-lp/',
+    techs: ['Angular.js', 'SASS', 'Meterial UI'],
+    noFollow: true,
+    img: {
+      path: require('./portfolio_images/personalPage.png'),
+      alt: 'cover image'
     }
   }
 ];
