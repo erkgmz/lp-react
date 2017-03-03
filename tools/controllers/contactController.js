8673b430df9dae860162e4f3d72583aae0d48564
Erik Gomez <egoxiv@gmail.com>
2017-03-02 22:04:27 -0800

    Changes from_address in contact controller

--- tools/controllers/contactController.js
+++ tools/controllers/contactController.js
@@ -2,20 +2,20 @@ import sendgrid from 'sendgrid';
 
 require('dotenv').config();
 
-let contactController = {};
-
 /* eslint-disable no-console */
+
+let contactController = {};
 
 contactController.sendMessage = (req, res) => {
   let {name, email, message} = req.body;
-
   let helper = sendgrid.mail;
-  let from_email = new helper.Email(email);
-  let to_email = new helper.Email(process.env.SENDGRID_EMAIL);
-  let subject = `You've received a message from ${name}!`;
-  let content = new helper.Content('text/plain', message);
-  let mail = new helper.Mail(from_email, subject, to_email, content);
-  console.log(mail);
+
+  let from_email = new helper.Email(process.env.SENDGRID_EMAIL);
+  let to_email   = new helper.Email(process.env.SENDGRID_EMAIL);
+  let subject    = `${name} has sent you a message from your site`;
+  let content    = new helper.Content('text/plain', `${name} <${email}> has sent you a message:
+                                                     ${message}`);
+  let mail       = new helper.Mail(from_email, subject, to_email, content);
 
   let sg = sendgrid(process.env.SENDGRID_API_KEY);
   let request = sg.emptyRequest({
