de90d44e9041d60be85bc7e38165b6c123d32f43
Erik Gomez <egoxiv@gmail.com>
2017-03-10 19:42:40 -0800

    Adds new bool field in projects.

--- src/components/work/projects.js
+++ src/components/work/projects.js
@@ -4,6 +4,7 @@ const PROJECTS = [
     link: 'https://goo.gl/XaPTjw',
     techs: ['React', 'CSS Modules', 'ES6'],
     noFollow: false,
+    inDevelopment: true,
     img: {
       path: require('./portfolio_images/ksMarketing.png'),
       alt: 'team photo'
@@ -14,6 +15,7 @@ const PROJECTS = [
     link: 'https://ga-admissions.herokuapp.com/',
     techs: ['Mongo', 'Express', 'Angular.js', 'Node.js', 'Bootstrap'],
     noFollow: false,
+    inDevelopment: false,
     img: {
       path: require('./portfolio_images/generalAssembly.png'),
       alt: 'General Assembly logo'
@@ -24,6 +26,7 @@ const PROJECTS = [
     link: 'http://course-walkers.herokuapp.com/',
     techs: ['jQuery', 'Flexbox', 'Handlebars.js'],
     noFollow: false,
+    inDevelopment: false,
     img: {
       path: require('./portfolio_images/coursewalkers.png'),
       alt: 'Course Walkers logo'
@@ -34,6 +37,7 @@ const PROJECTS = [
     link: 'https://erkgmz.github.io/e-lp/',
     techs: ['Angular.js', 'SASS', 'Meterial UI'],
     noFollow: true,
+    inDevelopment: false,
     img: {
       path: require('./portfolio_images/personalPage.png'),
       alt: 'cover image'
