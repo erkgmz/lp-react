3314c558f8d17e94107178c85397e027a80f9e2c
Erik Gomez <e@erikgomez.co>
2017-03-15 16:31:29 -0700

    configures buildHtml for ejs

--- tools/buildHtml.js
+++ tools/buildHtml.js
@@ -11,13 +11,10 @@ fs.readFile('src/index.ejs', 'utf-8', (err, markup) => {
 
   const $ = cheerio.load(markup);
 
-  // $('head').prepend(`<script src="https://code.jquery.com/jquery-2.2.4.min.js" integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=" crossorigin="anonymous"></script>`);
-
-  // $.html(),
-  fs.writeFile('dist/index.html', $.html(), 'utf-8', err => {
+  fs.writeFile('dist/static/index.html', $.html(), 'utf-8', err => {
     if(err) {
       return console.log(err);
     }
-    console.log('index.html written to /dist'.green);
+    console.log('index.html written to /dist/static/'.green);
   });
 });
