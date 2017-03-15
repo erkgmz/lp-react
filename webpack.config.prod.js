a4b9cc188e2fba2d12a23e1c6a7e0ba54e6a65f4
Erik Gomez <e@erikgomez.co>
2017-03-15 16:29:50 -0700

    Adds static dir to output

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -11,7 +11,7 @@ const GLOBALS = {
 
 export default {
   resolve: {
-    extensions: ['', '.js', '.jsx', '.json']
+    extensions: ['', '.js', '.json']
   },
   debug: true,
   devtool: 'source-map',
@@ -21,7 +21,7 @@ export default {
   },
   target: 'web',
   output: {
-    path: path.resolve(__dirname, 'dist'), // Note: Physical files are only output by the production build task `npm run build`.
+    path: path.resolve(__dirname, 'dist/static/'), // Note: Physical files are only output by the production build task `npm run build`.
     publicPath: './',
     filename: '[name].bundle.js'
   },
