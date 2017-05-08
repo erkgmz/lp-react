2335a048e5b8dcda4c4c6fc26694125d6e5f370c
Erik Gomez <e@erikgomez.co>
2017-05-08 12:31:56 -0700

    Makes directory changes, work to experience.

--- src/components/experience/ExperiencePage.js
+++ src/components/experience/ExperiencePage.js
@@ -4,12 +4,12 @@ import {Link} from 'react-router';
 import Headline from '../common/Headline';
 import Cards from './Cards';
 
-import css from '../../css/sass/components/_workpage.scss'; // eslint-disable-line
+import css from '../../css/sass/components/_experiencepage.scss'; // eslint-disable-line
 import cards from '../../css/sass/components/_cards.scss'; // eslint-disable-line
 
-const WorkPage = () => {
+const ExperiencePage = () => {
   return (
-    <article className={css.workpage}>
+    <article className={css.experience}>
       <Headline text="Experience" />
       <p>Below are a few of my web development projects. <Link to="/contact">Send me a message</Link> if you'd like to see more or have any questions.</p>
       <Cards styles={cards} />
@@ -17,4 +17,4 @@ const WorkPage = () => {
   );
 };
 
-export default WorkPage;
+export default ExperiencePage;
