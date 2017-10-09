8ffa4f4756ab3290e6f4f3c97c57e3d4e795c907
Erik Gomez <e@erikgomez.co>
2017-10-09 12:28:02 -0700

    Adds new copy in homepage

--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -12,12 +12,13 @@ const HomePage = () => {
   return (
     <article className={css.homepage}>
       <div>
-        <Headline text="Hi, I'm Erik." />
-        {/*<Byline text="Front-End Developer at The AdExchange Group in Irvine, CA." />*/}
-        <p>Front-End Developer at The AdExchange Group in Irvine, CA.</p>
+        <Headline text="HOW CAN I HELP?" />
+        {/* <Byline text="See how a Front-End developer can be the solution to your marketing needs." /> */}
+        <p>I am a Front-End developer with marketing, design & ecommerce experience.</p>
       </div>
       <div className={css.ctaContainer}>
         <PrimaryCTA path="/work" text="VIEW MY WORK" />
+        <PrimaryCTA path="/contact" text="MESSAGE ME" />
       </div>
     </article>
   );
