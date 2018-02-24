c017a83217fdd7d2c95ac51326ff7fea33783d8b
Erik Gomez <e@erikgomez.co>
2018-02-24 15:18:30 -0800

    Fix typo in workpage

--- src/components/work/WorkPage.js
+++ src/components/work/WorkPage.js
@@ -16,7 +16,7 @@ const WorkPage = () => {
           <li>To launch that Shopify store</li>
           <li>To make your emails responsive and better looking on more devices (even Outlook!)</li>
           <li>Help with UX/UI design</li>
-          <li>A Front-End Developers to code up a fresh new website for you</li>
+          <li>A Front-End Developer to code up a fresh new website for you</li>
         </ul>
         <p>... then we need to talk</p>
         <div className={css.ctaWrap}>
