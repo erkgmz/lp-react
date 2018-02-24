7dd003857e8206166d1c31fe25683c9cda09aae0
Erik Gomez <e@erikgomez.co>
2018-02-24 14:24:27 -0800

    Mising semi colon

--- src/components/work/WorkPage.js
+++ src/components/work/WorkPage.js
@@ -6,35 +6,28 @@ import Tile from './Tile';
 import {projectsList} from './projects-list';
 import css from '../../css/sass/components/_workpage.scss'; // eslint-disable-line
 
-class WorkPage extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      tiles: projectsList.map( (project, key) => <Tile css={css} project={project} key={key} /> )
-    };
-  }
-
-  render() {
-    return (
-      <article className={css.workpage}>
-        <div className={css.copyBlock}>
-          <Headline text="Insert Your Project Here" />
-          <p>If you need...</p>
-          <ul>
-            <li>To launch that Shopify store</li>
-            <li>To make your emails looks better on more devices (even Outlook!)</li>
-            <li>Help with UX/UI design</li>
-            <li>A pro to code up a fresh new website for you</li>
-          </ul>
-          <p>... then we need to talk</p>
-          <div className={css.ctaWrap}>
-            <PrimaryCTA path="/contact" text="SEND A MESSAGE" />
-          </div>
+const WorkPage = () => {
+  return (
+    <article className={css.workpage}>
+      <div className={css.copyBlock}>
+        <Headline text="Insert Your Project Here" />
+        <p>If you need...</p>
+        <ul>
+          <li>To launch that Shopify store</li>
+          <li>To make your emails responsive and better looking on more devices (even Outlook!)</li>
+          <li>Help with UX/UI design</li>
+          <li>A Front-End Developers to code up a fresh new website for you</li>
+        </ul>
+        <p>... then we need to talk</p>
+        <div className={css.ctaWrap}>
+          <PrimaryCTA path="/contact" text="SEND A MESSAGE" />
         </div>
-        <div className={css.tiles}>{this.state.tiles}</div>
-      </article>
-    );
-  }
-}
+      </div>
+      <div className={css.tiles}>
+        {projectsList.map( (project, key) => <Tile css={css} project={project} key={key} /> )}
+      </div>
+    </article>
+  );
+};
 
 export default WorkPage;
