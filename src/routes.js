06d367ac6994fdc78f5fa6411759ed1463f585cb
Erik Gomez <e@erikgomez.co>
2018-05-20 15:34:24 -0700

    Remove deprecated pages

--- src/routes.js
+++ src/routes.js
@@ -3,12 +3,9 @@ import { Route, IndexRoute } from 'react-router';
 
 import App from './components/App'; // eslint-disable-line import/no-named-as-default
 import HomePage from './components/home/HomePage';
-// import AboutPage from './components/about/AboutPage';
-// import ExperiencePage from './components/experience/ExperiencePage';
 import WorkPage from './components/work/WorkPage';
 import ContactPage from './components/contact/ContactPage';
 import ErrorPage from './components/error/ErrorPage';
-// import ServicesPage from './components/services/ServicesPage';
 
 export default (
   <Route path="/" component={App}>
