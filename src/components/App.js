4b29bb08eab74ef4e6c251adb632a8a811589235
Erik Gomez <e@erikgomez.co>
2018-07-02 23:03:52 -0700

    Remove vars

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,9 @@
 import React, {PropTypes} from 'react';
 import Header from './header/Header';
-import SocialIcons from './socialIcons/SocialIcons';
 import Footer from './footer/footer';
 import css from '../css/sass/components/_base.scss'; // eslint-disable-line
 
 const App = ({ children }) => {
-  let date = new Date();
-  let year = date.getFullYear();
-
   return (
     <div className={css.wrapper}>
       <Header />
