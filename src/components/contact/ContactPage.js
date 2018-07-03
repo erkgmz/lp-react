34b435ff06b4769eed3028bf4d0ee5103f4f3670
Erik Gomez <e@erikgomez.co>
2018-07-02 23:06:08 -0700

    Cleanup indentation

--- src/components/contact/ContactPage.js
+++ src/components/contact/ContactPage.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import Headline from '../common/Headline';
-import SocialIcons from '../socialIcons/SocialIcons';
 import Form from './Form';
 import css from '../../css/sass/components/_contact.scss'; // eslint-disable-line
 
@@ -9,10 +8,8 @@ const ContactPage = () => {
     <article className={css.contact}>
       <div className={css.copyblock}>
         <Headline text="LET'S CHAT"/>
-        <p>
-          I'm happy to answer any questions you have and would love to hear about your next project.
-          Just tell me about it and I'll be in touch.
-        </p>
+        <p>I'm happy to answer any questions you have and would love to hear
+        about your next project. Just tell me about it and I'll be in touch.</p>
       </div>
       <div className={css.formWrap}>
         <Form />
