0691d0530013f2491a0ca6927b607dd228a49551
Erik Gomez <e@erikgomez.co>
2018-09-22 11:21:31 -0700

    Add redirect

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
-/*eslint-disable import/default */
-import 'babel-polyfill';
-import React from 'react';
-import { render } from 'react-dom';
-import { Router, browserHistory } from 'react-router';
-import routes from './routes';
+window.location.replace('https://www.linkedin.com/in/erkgmz/');
+// /*eslint-disable import/default */
+// import 'babel-polyfill';
+// import React from 'react';
+// import { render } from 'react-dom';
+// import { Router, browserHistory } from 'react-router';
+// import routes from './routes';
 
-require('../favicon.ico');
-require('./assets/code_bg_dark@3x.jpg');
+// require('../favicon.ico');
+// require('./assets/code_bg_dark@3x.jpg');
 
-let root = document.getElementById('app');
-render(<Router history={browserHistory} routes={routes} />, root);
+// let root = document.getElementById('app');
+// render(<Router history={browserHistory} routes={routes} />, root);
